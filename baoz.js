41f8adf2c3fe71314d9a9e930845607d88858a81
friskfly <friskfly@gmail.com>
2013-07-21 10:50:42 +0800

    更新第二版
    更新内容：
    v0.1.2
    解决了第一版中当楼层超过52楼时只能加载到52楼的问题
    解决帖子之间跳转时无法滚动的问题。
    优化性能，减少重复计算，定位更精准。

--- baoz.js
+++ baoz.js
@@ -3,25 +3,28 @@
 	baoz_assist_storage = localStorage.getItem(prefix)
 	if (!baoz_assist_storage) baoz_assist_storage = "{}"
 	baoz_assist_storage = JSON.parse(baoz_assist_storage)
-
 	document.addEventListener('click', function(e) {
-		var srcElement = e.target || e.srcElement
-		while (srcElement && srcElement.tagName.toLowerCase() != "a") srcElement = srcElement.parentElement
-		if (srcElement) {
-			var href = srcElement.getAttribute("href")
-			var tid = /(\d+)/.exec(href)
-			tid = tid && tid[0]
-			tid && setTimeout(function() {
-				var tidScroll = baoz_assist_storage[tid]
-				tidScroll && _scrollTo(tidScroll)
-			}, 1000)
-			tid && setTimeout(function() {
-				document.querySelector(".content .view_body div").addEventListener('mousewheel', _debounce(function(e) {
-					baoz_assist_storage[tid] = this.scrollTop
-					localStorage.setItem(prefix, JSON.stringify(baoz_assist_storage))
-				}, 300), false)
-			}, 1500)
-		}
+		setTimeout(function() {
+			var srcElement = e.target || e.srcElement
+			while (srcElement && srcElement.tagName.toLowerCase() != "a" && srcElement.tagName.toLowerCase() != "ul") srcElement = srcElement.parentElement
+			if (srcElement && srcElement.tagName.toLowerCase() == "a") {
+				var href = srcElement.getAttribute("href")
+				var tid = /(\d+)/.exec(href)
+				tid = tid && tid[0]
+				tid && setTimeout(function() {
+					var tidScroll = baoz_assist_storage[tid]
+					tidScroll && _scrollTo(tidScroll)
+				}, 100)
+				tid && setTimeout(function() {
+					var content = document.querySelectorAll(".content")
+					scrollRole = content.item(content.length - 1).querySelector(".view_body div");
+					scrollRole.addEventListener('mousewheel', _debounce(function(e) {
+						baoz_assist_storage[tid] = this.scrollTop
+						localStorage.setItem(prefix, JSON.stringify(baoz_assist_storage))
+					}, 300), false)
+				}, 1000)
+			}
+		}, 1000)
 	}, false)
 
 	function _debounce(func, wait, immediate) {
@@ -42,14 +45,27 @@
 	};
 
 	function _scrollTo(x) {
-		var scroll = 0
-		var i = 100;
+		var scroll = 0,
+			i = 100,
+			content = document.querySelectorAll(".content")
+			scrollRole = content.item(content.length - 1).querySelector(".view_body div");
+		//scrollRole = document.querySelector(".content[style='top: 0px;'] .view_body div"),  这种选择器能工作但不太靠谱
+
 		function __scrollTo() {
-			document.querySelector(".content .view_body div").scrollTop = scroll
+			scrollRole.scrollTop = scroll
+			scrollHeight = scrollRole.scrollHeight - scrollRole.clientHeight
 			scroll += i
 			i += 100
-			if (scroll < x) setTimeout(__scrollTo, 100)
-			else document.querySelector(".content .view_body div").scrollTop = x
+			if (scrollHeight >= x) {
+				if (scroll < x) setTimeout(__scrollTo, 100)
+				else scrollRole.scrollTop = x
+			} else {
+				if (scroll < scrollHeight) setTimeout(__scrollTo, 100)
+				else {
+					scrollRole.scrollTop = scrollHeight
+					setTimeout(__scrollTo, 200)
+				}
+			}
 		}
 		__scrollTo()
 	}
